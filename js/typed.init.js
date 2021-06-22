464b995bf7429c93d03b03df0df05e2dd3f407a3
shegemm5252 <49652308+shegemm5252@users.noreply.github.com>
2021-06-22 07:49:01 +0100

    first commit

---
+++ js/typed.init.js
@@ -0,0 +1,10 @@
+
+// Typed Text
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 200,
+    loop: true,
+    strings: [
+        'Mobile Developer', 'Web Developer', 'Database Administrator',
+    ],
+});
